9931c46e27fdbace28c9709e650f64827a0552ec
laxyyza <laxyy420@proton.me>
2024-10-26 14:37:09 +0200

    Fix submodules

--- client/lib/wa/wa/include/wa_wayland_egl.h
+++
@@ -1,9 +0,0 @@
-#ifndef _WA_WAYLAND_EGL_H_
-#define _WA_WAYLAND_EGL_H_
-
-#include "wa.h"
-
-bool wa_window_egl_init(wa_window_t* window);
-void wa_window_egl_cleanup(wa_window_t* window);
-
-#endif // _WA_WAYLAND_EGL_H_
