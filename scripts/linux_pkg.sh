6019220402b8121f0544f006250a14725e0dc8a9
laxyyza <laxyy420@proton.me>
2024-11-07 17:19:49 +0200

    scripts: Update linux packaging

--- scripts/linux_pkg.sh
+++ scripts/linux_pkg.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
+VERSION=$(cat VERSION)
+
 BUILD_DIR=build
-STAG_DIR=$BUILD_DIR/wa_game
+STAG_NAME=wa_game_linux$VERSION
+STAG_DIR=$BUILD_DIR/$STAG_NAME
 SHADERS_DIR=$STAG_DIR/client/src/shaders
 CLIENT_PATH=$BUILD_DIR/client/wa_game
 SERVER_PATH=$BUILD_DIR/server/server
-OUT_NAME=wa_game_linux.tar.zst
+BIN_DIR=$STAG_DIR/bin
+
+OUT_TAR=wa_game_linux$VERSION.tar
 
 ninja -C $BUILD_DIR
-mkdir -p $SHADERS_DIR $STAG_DIR/lib
+mkdir -p $SHADERS_DIR $STAG_DIR/lib $BIN_DIR
 
 cp -rv res $STAG_DIR
 cp -v client/src/shaders/* $SHADERS_DIR
 ./scripts/copylibs.py $CLIENT_PATH $STAG_DIR/lib
 strip $SERVER_PATH $CLIENT_PATH
-cp -v $CLIENT_PATH $SERVER_PATH $STAG_DIR
+cp -v $CLIENT_PATH $SERVER_PATH $BIN_DIR
+cp -v scripts/wa_game.sh $STAG_DIR
 
-cd $BUILD_DIR && tar --zstd -cvf $OUT_NAME wa_game
+cd $BUILD_DIR && tar -cvf $OUT_TAR $STAG_NAME
 
+zstd $OUT_TAR
+xz -z -k $OUT_TAR
+gzip -k $OUT_TAR
---
+++ scripts/wa_game.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+OUT=client/log.txt
+EXE=bin/wa_game
+
+exec $EXE &> $OUT
