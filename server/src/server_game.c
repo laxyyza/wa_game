1681eb4ecad66fb568828487b9e75289cd2b1cb8
laxyyza <laxyy420@proton.me>
2024-11-22 14:31:55 +0200

    server: Mark player health's packet as Important

--- server/src/server_game.c
+++ server/src/server_game.c
@@ -125,7 +125,7 @@ on_player_damaged(cg_player_t* target_player, cg_player_t* attacker_player, serv
 	}
 
 	GHT_FOREACH(client_t* client, clients, {
-		ssp_segbuf_add(&client->udp_buf, NET_UDP_PLAYER_HEALTH, sizeof(net_udp_player_health_t), health);
+		ssp_segbuf_add_i(&client->udp_buf, NET_UDP_PLAYER_HEALTH, sizeof(net_udp_player_health_t), health);
 		if (move)
 		{
 			ssp_segbuf_add_i(&client->udp_buf, NET_UDP_PLAYER_MOVE, sizeof(net_udp_player_move_t), move);
